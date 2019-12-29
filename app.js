f5e441d2519a8d48406b06f96afb7233297f7a0b
pratiksubhedar <35028066+pratiksubhedar@users.noreply.github.com>
2019-12-30 03:01:36 +0530

    added cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 // var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var weatherRouter = require('./routes/weather');
@@ -17,6 +18,7 @@ app.use(logger('dev'));
 app.use(express.json()); // to enable reading of the request.body object in case of a POST request.
 // app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
